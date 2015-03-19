c5bad17a6fa5dc0f801be7cca19e8630acf08a16
0neblock <s3471910@student.rmit.edu.au>
2015-03-19 18:48:27 +1100

    Options

--- getRandom/index.php
+++ getRandom/index.php
@@ -1,6 +1,8 @@
 <?php
     if(isset($_GET['data'])){
         $data = json_decode($_GET['data']);
+        $numOptions =sizeof($data["options"]);
+        echo $numOptions;
         echo json_encode($data);
     }
 
