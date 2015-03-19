567ff22da0d29764092b31af0665d012743854f2
0neblock <s3471910@student.rmit.edu.au>
2015-03-19 18:57:29 +1100

    stdClass to array

--- getRandom/index.php
+++ getRandom/index.php
@@ -2,8 +2,9 @@
     if(isset($_GET['data'])){
         $data = json_decode($_GET['data']);
         print_r($data);
-       // $numOptions = sizeof($data["options"]);
-       // echo $numOptions;
+        $data = (array)$data;
+        $numOptions = sizeof($data["options"]);
+        echo $numOptions;
         echo json_encode($data);
     }
 
